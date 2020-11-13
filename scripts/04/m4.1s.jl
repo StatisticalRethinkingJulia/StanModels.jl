13729352293185974fc809bf5fd9e4a2389797bb
Rob J Goedman <goedman@mac.com>
2020-11-13 14:17:58 -0700

    Rel 2.1.1

--- scripts/04/m4.1s.jl
+++ scripts/04/m4.1s.jl
@@ -3,7 +3,7 @@ using StanSample, MCMCChains, CSV
 df = filter(row -> row[:age] >= 18, 
   CSV.read(joinpath(@__DIR__, "..", "..", "data", "Howell1.csv"), DataFrame))
 
-m4_1s = "
+stan4_1 = "
 // Inferring a Rate
 data {
   int N;
@@ -23,17 +23,18 @@ model {
 }
 ";
 
-m_4_1s = SampleModel("m4.1s", m4_1s);
+m4_1s = SampleModel("m4.1s", stan4_1);
 
 m4_1_data = Dict("N" => length(df[!, :height]), "h" => df[!, :height]);
 
-rc = stan_sample(m_4_1s, data=m4_1_data);
+rc4_1s = stan_sample(m4_1s, data=m4_1_data);
 
-if success(rc)
-  chn = read_samples(m_4_1s; output_format=:mcmcchains)
+if success(rc4_1s)
+  #chn4_1s = read_samples(m4_1s; output_format=:mcmcchains)
   # Update parameter names
-  chn = replacenames(chn, Dict("mu" => "μ", "sigma" => "σ"))
-  chn |> display
-end
+  #chn4_1s = replacenames(chn4_1s, Dict("mu" => "μ", "sigma" => "σ"))
+  #chn4_1s
 
-# chn = Chains(val, ["a", "b", "c", "d", "e"])
+  part4_1s = read_samples(m4_1s; output_format=:particles)
+  part4_1s |> display
+end
